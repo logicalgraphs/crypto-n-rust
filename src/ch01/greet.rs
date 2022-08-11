0a5903133790727fd15618c3c5f7a3f2cbc319e4
geophf <dauclair@hotmail.com>
2022-08-11 07:49:43 -0400

    Switched from if-syntax to match-syntax. I wish I knew how to pattern-match guard on function arguments!

--- src/ch01/greet.rs
+++ src/ch01/greet.rs
@@ -6,11 +6,12 @@ fn main() {
    let args: Vec<_> = env::args().collect();
    let (_, names) = args.split_at(1);
     
-   if names.len() > 0  {
-      for name in names {
-         println!("Hello, {}!", name);
-      }
-   } else {
-      println!("Whom?");
-  }
+   match names.len() {
+     0 => { println!("Whom?"); }
+     _ => {
+         for name in names {
+            println!("Hello, {}!", name);
+         }
+     }
+   };
 }
