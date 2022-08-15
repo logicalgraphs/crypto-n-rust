b5d1142e02e8d80eb35b6ef4563d7d07cdce5f6b
geophf <dauclair@hotmail.com>
2022-08-14 21:21:48 -0400

    Error-handling now graceful. Transformation now broken out by line.

--- src/ch01/summer.rs
+++ src/ch01/summer.rs
@@ -9,7 +9,10 @@ fn main() {
    let args: Vec<_> = env::args().collect();
    let (_, args) = args.split_at(1);
 
-   let nums: Vec<f32> = args.iter().map(|n| n.parse().unwrap()).collect();
+   let nums: Vec<f32> =
+          args.iter()
+              .map(|n| n.parse().expect(&(n.to_owned() + " isn't a number")))
+              .collect();
    let sum: f32 = nums.iter().sum();
    println!("The sum of {:?} is {:?}", nums, sum);
 }
