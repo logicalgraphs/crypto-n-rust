1065e960726e585cd98719232ac5bbc4e67ebfe7
geophf <dauclair@hotmail.com>
2022-08-30 15:42:12 -0400

    Separated number parsing from fetching command line arguments

--- src/ch03/utils.rs
+++ src/ch03/utils.rs
@@ -7,9 +7,11 @@ pub fn get_args() -> Vec<String> {
 }
 
 pub fn get_nums() -> Vec<f32> {
-   let nums: Vec<f32> =
-      get_args().iter()
-         .map(|n| n.parse().expect(&(n.to_owned() + " isn't a number")))
-         .collect();
-   nums
+   parse_nums(get_args())
+}
+
+pub fn parse_nums(strs: Vec<String>) -> Vec<f32> {
+   strs.iter()
+       .map(|n| n.parse().expect(&(n.to_owned() + " isn't a number")))
+       .collect()
 }
