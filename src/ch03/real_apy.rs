c59953f2ed23f8ddba42d0d1f78e90ab8d61127f
geophf <dauclair@hotmail.com>
2022-08-30 22:45:39 -0400

    Added debug-mode: `--spew` to show all calculations.

--- src/ch03/real_apy.rs
+++ src/ch03/real_apy.rs
@@ -3,29 +3,50 @@
 mod utils;
 
 fn usage() {
-   println!("\n$ ./real_apy <supply> <borrow> <net_apy>");
+   println!("\n$ ./real_apy [--spew] <supply> <borrow> <net_apy>");
    println!("\n\tcomputes real APY from leveraged assets.");
    println!("\nFor example:");
    println!("\n\t$ ./real_apy 17206.37 9742.39 1.81");
-   println!("\n\tYour real APY is 6.39%\n");
+   println!("\n\tYour real APY is 6.39%");
+   println!("\n(add \"--spew\" to see all the glorious calculations)\n");
 }
 
 fn main() {
-   let nums = utils::get_nums();
+   let mut args = utils::get_args();
+   let spew: bool = args.len() > 0 && args[0] == "--spew";
+   if spew { args.remove(0); }
+   let nums = utils::parse_nums(args);
    if let [supply, borrow, net] = nums.as_slice() {
-      let real_r = compute_real_r(*supply, *borrow, *net / 100.0);
+      let real_r = compute_real_r(*supply, *borrow, *net / 100.0, spew);
       println!("Your real APY is {}%", real_r * 100.0);
    } else {
       usage();
    }
 }
 
-fn compute_real_r(supply: f32, borrow: f32, net_r: f32) -> f32 {
+fn compute_real_r(supply: f32, borrow: f32, net_r: f32, spew: bool) -> f32 {
    let principal = supply - borrow;
    let leverage = supply + borrow;
    let eoy_leverage = leverage * net_r.exp();
    let distribution = eoy_leverage - leverage;
    let eoy_principal = principal + distribution;
    let real_r = (eoy_principal / principal).ln();
+   if spew { spewage(supply, borrow, net_r, principal, leverage, eoy_leverage,
+                     distribution, eoy_principal); }
    real_r
+}
+
+fn spewage(supp: f32, borr: f32, rate: f32, princ: f32,
+           lev: f32, eoyl: f32, dist: f32, eoyp: f32) {
+   println!("Given:");
+   println!("\tSupply (S):                               {}", supp);
+   println!("\tBorrow (B):                               {}", borr);
+   println!("\tRate (R):                                 {}%\n", rate * 100.0);
+   println!("We compute:");
+   println!("\tPrincipal (S - B = P):                    {}", princ);
+   println!("\tLeverage  (S + B = L):                    {}", lev);
+   println!("\tEnd-of-year leverage (L * exp(R) = EOYL): {}", eoyl);
+   println!("\tDistribution (EOYL - L = D):              {}", dist);
+   println!("\tEnd-of-year principal (P + D = EOYP):     {}", eoyp);
+   println!("\t... and the real rate is ln(EOYP/P)\n");
 }
--- src/ch03/utils.rs
+++ src/ch03/utils.rs
@@ -2,16 +2,26 @@ use std::env;
 
 pub fn get_args() -> Vec<String> {
    let cmd_lin: Vec<String> = env::args().collect();
-   let (_, args) = cmd_lin.split_at(1);
-   args.to_vec()
+   let (_, args) = ht::<String>(cmd_lin);
+   args
 }
 
 pub fn get_nums() -> Vec<f32> {
    parse_nums(get_args())
 }
 
+// parse_nums() Influenced by the following overflows:
+
+// https://stackoverflow.com/questions/27043268/convert-a-string-to-int
+// https://stackoverflow.com/questions/23100534/how-to-sum-the-values-in-an-array-slice-or-vec-in-rust
+
 pub fn parse_nums(strs: Vec<String>) -> Vec<f32> {
    strs.iter()
        .map(|n| n.parse().expect(&(n.to_owned() + " isn't a number")))
        .collect()
+}
+
+pub fn ht<T: std::clone::Clone>(list: Vec<T>) -> (Option<T>, Vec<T>) {
+   let (f, t) = list.split_at(1);
+   (f.to_vec().pop(), t.to_vec())
 }
