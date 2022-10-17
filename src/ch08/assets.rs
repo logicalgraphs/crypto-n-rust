641e4acc74f5c9faf4974d2f74e2125055c9940b
geophf <dauclair@hotmail.com>
2022-10-17 17:40:05 -0400

    A bit o' MUTABLE(??) functional programming? Don't `@` me. Eheh. 😅

--- src/ch08/assets.rs
+++ src/ch08/assets.rs
@@ -1,11 +1,16 @@
+use std::{
+   collections::HashSet,
+   path::Path
+};
+
 use book::{
    utils::get_args,
    file_utils::lines_from_file,
    csv_utils::print_csv,
-   list_utils::head
+   list_utils::{head,tail}
 };
 
-use crypto::types::Asset;
+use crypto::types::assets::{Asset,read_csv_asset,merge_assets};
 
 fn usage() {
    println!("\n./assets <assets CSV file>");
@@ -15,7 +20,30 @@ fn usage() {
 fn main() {
    if let Some(filename) = head(get_args()) {
       println!("Processing {}", filename);
+      parse_n_print(filename);
    } else {
       usage();
    }
+}
+
+fn parse_n_print(file: impl AsRef<Path>) {
+   let lines = tail(lines_from_file(file));
+   let mut assets = HashSet::new();
+   lines.iter().for_each(|line| parse_n_add(line, &mut assets));
+   println!("asset,amount,quote");
+   assets.iter().for_each(print_csv);
+}
+
+fn parse_n_add(line: &String, assets: &mut HashSet<Asset>) {
+   match read_csv_asset(line) {
+      Ok(asset) => update(assets, asset),
+      Err(msg) => println!("{}", msg)
+   }
+}
+
+fn update(assets: &mut HashSet<Asset>, a: Asset) {
+   assets.replace(match assets.get(&a) {
+      Some(d) => merge_assets(d, a),
+      None    => a
+   });
 }
--- src/libs/crypto/types/assets.rs
+++ src/libs/crypto/types/assets.rs
@@ -1,8 +1,49 @@
+use std::{
+   cmp::Ordering,
+   hash::{Hash,Hasher}
+};
+
+use book::csv_utils::CsvWriter;
+
 #[derive(Debug, Clone)]
 pub struct Asset {
    token: String,
    amount: f32,
    quote: f32
+}
+
+impl CsvWriter for Asset {
+   fn as_csv(&self) -> String { csv(self) }
+}
+
+impl Hash for Asset {
+   fn hash<H: Hasher>(&self, state: &mut H) {
+      self.token.hash(state);
+   }
+}
+
+impl PartialEq for Asset {
+   fn eq(&self, other: &Self) -> bool {
+      self.token == other.token
+   }
+}
+
+impl Eq for Asset {}
+
+impl Ord for Asset {
+   fn cmp(&self, other: &Self) -> Ordering {
+      self.token.cmp(&other.token)
+   }
+}
+
+impl PartialOrd for Asset {
+   fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+      Some(self.cmp(other))
+   }
+}
+
+fn csv(asset: &Asset) -> String {
+   format_args!("{},{},{}",asset.token, asset.amount, asset.quote).to_string()
 }
 
 pub fn mk_asset(token: String, amount: f32, quote: f32) -> Asset {
@@ -17,11 +58,11 @@ pub fn parse_asset(tok: &str, amt: &str, quot: &str)
    Ok(Asset { token, amount, quote })
 }
 
-pub fn merge_assets(a1: Asset, a2: Asset) -> Asset {
-   let token = a1.token;
-   let amount = a1.amount + a2. amount;
-   let quote = (a1.quote + a2.quote) / 2.0;
-   Asset { token, amount, quote }
+pub fn merge_assets(a1: &Asset, a2: Asset) -> Asset {
+   let token = &a1.token;
+   let amount = a1.amount + a2.amount;
+   let quote = (a1.quote * a1.amount + a2.quote * a2.amount) / amount;
+   mk_asset(token.to_string(), amount, quote)
 }
 
 pub fn read_csv_asset(line: &String) -> Result<Asset, String> {
