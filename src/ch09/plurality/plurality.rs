ec5b0c3ebc5704ce4689d3fadbbc640881bd901f
geophf <dauclair@hotmail.com>
2022-12-27 07:39:54 -0500

    Explaining the plurality of plural-...ity ;)

--- src/ch09/plurality/plurality.rs
+++ src/ch09/plurality/plurality.rs
@@ -5,4 +5,12 @@ fn main() {
    println!("I made {}.", plural(1, trade));
    println!("I made {}.", plural(0, trade));
    println!("I made {} yesterdæg, 'cuz I GOT BIZY, FAM!", plural(327, trade));
+
+   let plur = "plural";
+   let plurs: String = plural(2, plur);
+   println!("\nBUT DID YOU KNOWWWWWWWW:
+
+That the plural of '{plur}' is '{plurs}'?
+
+Try it out! 😎 ");
 }
