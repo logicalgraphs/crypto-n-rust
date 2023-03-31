98d777649e4708a795df2ffe1a14cfaceeec218d
geophf <dauclair@hotmail.com>
2023-03-31 09:13:46 -0400

    Reports don't shout the title. This simplification had wide-reacting effects.

--- src/libs/book/string_utils.rs
+++ src/libs/book/string_utils.rs
@@ -8,12 +8,6 @@ pub fn quot(s: &str) -> String {
    format!("\"{s}\"")
 }
 
-pub fn str_string(str: &str) -> String {
-   let mut ans = String::new();
-   ans.push_str(str);
-   ans
-}
-
 pub fn plural(n: u32, noun: &str) -> String {
    let s = if n == 1 { "" } else { "s" };
    format!("{n} {noun}{s}")
