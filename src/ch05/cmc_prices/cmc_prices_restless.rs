33a852b5ad47e5398482907e21a5e5bcf699f0e7
geophf <dauclair@hotmail.com>
2023-07-29 14:31:44 -0400

    Update cmc_prices to read JSON directly from the CMC REST endpoint.

---
+++ src/ch05/cmc_prices/cmc_prices_restless.rs
@@ -0,0 +1,30 @@
+// given a filename, we parse the JSON and spit out the prices
+
+// Easy-peasy!
+
+use book::{
+   utils::get_args,
+   list_utils::head,
+};
+
+use crypto::{
+   types::coins::print_all_coins,
+   json_utils::parse_coins
+};
+
+use std::fs::read_to_string;
+
+fn usage() {
+   println!("\n./cmc_prices <filename>");
+   println!("\n\tReads <filename> and extracts tokens and their prices.\n");
+}
+
+fn main() {
+   if let Some(filename) = head(&get_args()) {
+      let data = read_to_string(filename).expect("Unable to read file");
+      let coins = parse_coins(&data);
+      print_all_coins(coins);
+   } else {
+      usage();
+   }
+}
