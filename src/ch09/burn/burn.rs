3381cb83bfce99fa1f6af86500cdbc918a7eab0a
geophf <dauclair@hotmail.com>
2023-08-10 03:02:10 -0400

    Incorporated meth to get LSD exchange rates and unbonding periods

--- src/ch09/burn/burn.rs
+++ src/ch09/burn/burn.rs
@@ -2,9 +2,15 @@ use bunsen::entries::{
    OrderBook, parse_orderbook, buy, report_buy, report_roi
 };
 
+use meth::{
+   stride::fetch_stride_lsds,
+   types::{exchange_rate,token}
+};
+
 use book::{
    csv_utils::print_csv,
    file_utils::read_file,
+   list_utils::assoc_list,
    utils::get_args
 };
 
@@ -12,7 +18,7 @@ fn usage() {
    let url = "https://api.kujira.app/api/coingecko/orderbook";
    let tick = "ticker_id=LOCAL_USK&depth=10";
 
-   println!("\n./burn <ntoks> <exchange rate> <burn period> <book>");
+   println!("\n./burn <ntoks> <book>");
    println!("\tParses <book> into an order book, then buys base with <ntoks>");
    let msg = "from <exchange rate> and <burn period>";
    println!("\tWe compute burn ROI and APR {msg}.\n");
@@ -22,16 +28,20 @@ fn usage() {
 fn main() -> Result<(), String> {
    let args = get_args();
    let mut success = false;
-   if let [amt, ex_rate, period, filename] = args.as_slice() {
+   if let [amt, filename] = args.as_slice() {
       let amount: f32 = amt.parse().expect(&format!("{amt} is not a number"));
-      let rate: f32 =
-         ex_rate.parse().expect(&format!("{ex_rate} is not a rate"));
-      let burn: f32 =
-         period.parse().expect(&format!("{period} is not a number of days."));
       let file = read_file(&filename);
       let book = parse_orderbook(&file)?;
-      success = true;
-      reportage(amount, rate, burn, &book);
+      let lsds1 = fetch_stride_lsds()?;
+      let lsds = assoc_list(lsds1, token);
+      if let Some(lsd) = lsds.get(&book.base) {
+         let rate = exchange_rate(lsd);
+         let burn = lsd.unbond;
+         success = true;
+         reportage(amount, rate, burn as f32, &book);
+      } else {
+         panic!("Cannot find LSD for {}", book.base)
+      }
    }
    if !success {
       usage();
@@ -44,5 +54,7 @@ fn reportage(amount: f32, rate: f32, burn: f32, book: &OrderBook) {
 
    let purchase = buy(book, amount);
    println!("\n{}", report_buy(&book, amount, &purchase));
-   println!("\n{}", report_roi(rate, burn, &purchase));
+
+   println!("\n{}, exchange rate: {rate}, unbonding period: {burn}", book.base);
+   println!("{}", report_roi(rate, burn, &purchase));
 }
