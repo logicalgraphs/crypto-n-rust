9ee527fc60618dc64eee22092a3457c3dcb77d15
geophf <dauclair@hotmail.com>
2024-01-11 22:30:12 -0500

    ORCA liquidation reports now sliced-n-diced by bids and assets

--- src/ch10/orca/liquidations.rs
+++ src/ch10/orca/liquidations.rs
@@ -14,6 +14,8 @@ ORCA liquidations follow the following format per liquidation
 The very next line is the next liquidation.
 */
 
+// ----- Imports -------------------------------------------------------
+
 use std::collections::HashMap;
 
 use chrono::naive::NaiveDate;
@@ -32,6 +34,8 @@ use crypto::{
    types::usd::{USD,no_monay,mk_usd,sum_usd}
 };
 
+// ----- Main -------------------------------------------------------
+
 fn usage() -> bool {
    println!("./cillaz <prices CSV> <liquidations LSV>");
    println!("\nSlices and dices liquidations on ORCA (by day and by market)");
@@ -44,7 +48,7 @@ fn main() {
       let prces = read_prices(&prices);
       let lines = lines_from_file(&liquids);
       let jours = process_liquidations_by_date(&prces, &lines);
-      print_by_days(&jours);
+      report(&jours);
       okay = true;
    }
 
@@ -52,7 +56,7 @@ fn main() {
    !okay && usage();
 }
 
-fn read_prices(file: &str) -> HashMap<String, USD> {
+fn read_prices(file: &str) -> Quotes {
    let (_date, lines) = extract_date_and_body(file);
    let mut ans = HashMap::new();
 
@@ -70,26 +74,56 @@ fn read_prices(file: &str) -> HashMap<String, USD> {
 
 type Market = (String, String);
 
-fn market(m: &Market) -> String {
+fn pair(m: &Market) -> String {
    let (asset, bid) = m;
    format!("{bid},{asset}")
 }
 
+fn market(m: &Market) -> String {
+   let (asset, bid) = m;
+   match str_opt(bid) {
+      Some(b) => format!("{b}{}", match str_opt(asset) {
+         Some(a) => format!("->{a}"),
+         None => "".to_string()
+      }),
+      None => asset.to_string()
+   }
+}
+
+fn str_opt(s: &str) -> Option<String> {
+   if s == "---" { None} else { Some(s.to_string()) }
+}
+
+fn erase_bid(m: &Market) -> Market {
+   let (a, _) = m;
+   (a.to_string(), "---".to_string())
+}
+
+fn erase_asset(m: &Market) -> Market {
+   let (_, b) = m;
+   ("---".to_string(), b.to_string())
+}
+
+fn id_market(m: &Market) -> Market {
+   m.clone()
+}
+
 type Amount = (usize, USD);
 type Liquidations = HashMap<Market, Amount>;
+type LiquidationsByDate = HashMap<NaiveDate, Liquidations>;
+type Quotes = HashMap<String, USD>;
+type Lines = Vec<String>;
 
 // ----- processors --------------------------------------------------
 
-fn process_liquidations_by_date(prices: &HashMap<String, USD>,
-                                lines: &Vec<String>)
-   -> HashMap<NaiveDate, Liquidations> {
+fn process_liquidations_by_date(prices: &Quotes, lines: &Lines)
+   -> LiquidationsByDate {
    let mut ans = HashMap::new();
    process_liqs(prices, lines, &mut ans);
    ans
 }
 
-fn process_liqs(prices: &HashMap<String, USD>, lyns: &Vec<String>,
-                ans: &mut HashMap<NaiveDate, Liquidations>) {
+fn process_liqs(prices: &Quotes, lyns: &Lines, ans: &mut LiquidationsByDate) {
    if let Some((n, date, market, amt)) = process_liquidation(prices, lyns) {
       let day = ans.entry(date).or_insert(HashMap::new());
       let amount = day.entry(market).or_insert((0, no_monay()));
@@ -99,7 +133,7 @@ fn process_liqs(prices: &HashMap<String, USD>, lyns: &Vec<String>,
    }
 }
 
-fn process_liquidation(prices: &HashMap<String, USD>, lines: &Vec<String>)
+fn process_liquidation(prices: &Quotes, lines: &Lines)
    -> Option<(usize, NaiveDate, Market, USD)> {
    if let Some((n, date)) = find_next_date(0, &lines) {
       let nl: Vec<String> = skip(n, &lines);
@@ -118,13 +152,12 @@ fn process_liquidation(prices: &HashMap<String, USD>, lines: &Vec<String>)
    } else { None }
 }
 
-fn skip(n: usize, lines: &Vec<String>) -> Vec<String> {
+fn skip(n: usize, lines: &Lines) -> Lines {
    let (_, t) = lines.split_at(n);
    t.to_vec()
 }
 
-fn find_next_date(idx: usize, lines: &Vec<String>)
-   -> Option<(usize, NaiveDate)> {
+fn find_next_date(idx: usize, lines: &Lines) -> Option<(usize, NaiveDate)> {
    if lines.is_empty() { None } else {
       if let (Some(h), t) = ht(lines) {
          let nidx = idx + 1;
@@ -138,8 +171,28 @@ fn find_next_date(idx: usize, lines: &Vec<String>)
 
 // ----- Printers --------------------------------------------------
 
-fn print_by_days(jours: &HashMap<NaiveDate, Liquidations>) {
-   println!("date,bid,asset,n,amount ($)");
+fn report(jours: &LiquidationsByDate) {
+   report_by("Market", id_market, jours);
+   report_by("Bid", erase_asset, jours);
+   report_by("Asset", erase_bid, jours);
+}
+
+fn report_by(title: &str, f: impl Fn(&Market) -> Market,
+             jours: &LiquidationsByDate) {
+   println!("\nORCA liquidations by {title}");
+   let rows = xform(&f, jours);
+   print_by_days(&rows);
+   let markets = by_market(&rows);
+   header("");
+   print_liquidations(None, &markets);
+}
+
+fn header(prefix: &str) {
+   println!("\n{prefix}bid,asset,n,amount ($),market");
+}
+
+fn print_by_days(jours: &LiquidationsByDate) {
+   header("date,");
    for (date,liq) in jours {
       print_liquidations(Some(&format!("{date}")), liq);
    }
@@ -149,6 +202,46 @@ fn print_liquidations(prefix: Option<&str>, liq: &Liquidations) {
    let pre = if let Some(pre1) = prefix { format!("{pre1},")
    } else { "".to_string() };
    for (mrket, (n,amt)) in liq {
-      println!("{pre}{},{n},{amt}", market(mrket));
+      println!("{pre}{},{n},{amt},{}", pair(mrket), market(mrket));
    }
+}
+
+// ----- Morphers --------------------------------------------------
+
+// without the dates, report the markets for the 7-days trailing
+
+fn by_market(jours: &LiquidationsByDate) -> Liquidations {
+   let mut markets: Liquidations = HashMap::new();
+   for (_, liqui) in jours {
+      for (mrk, amt) in liqui {
+         update_market(id_market, mrk, amt, &mut markets);
+      }
+   }
+   markets
+}
+
+fn xform(f: impl Fn(&Market) -> Market, jours: &LiquidationsByDate)
+   -> LiquidationsByDate {
+   let mut ans = HashMap::new();
+   for (day, liqs) in jours {
+      ans.insert(*day, xform1(&f, &liqs));
+   }
+   ans
+}
+
+fn xform1(f: impl Fn(&Market) -> Market, liqs: &Liquidations) -> Liquidations {
+   let mut ans = HashMap::new();
+   for (mkt, amt) in liqs {
+      update_market(&f, mkt, amt, &mut ans);
+   }
+   ans
+}
+
+fn update_market(f: impl Fn(&Market) -> Market, mkt: &Market,
+                 a: &Amount, markets: &mut Liquidations) {
+   let key = f(mkt);
+   let market = markets.entry(key.clone()).or_insert((0, no_monay()));
+   let (n1, amt1) = market;
+   let (n, amt) = a;
+   *market = (*n + *n1, sum_usd(amt, amt1));
 }
