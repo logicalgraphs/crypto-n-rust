325e3568400a6f78de4ac7f57dcc35cd9f1a96dc
geophf <dauclair@hotmail.com>
2024-02-01 10:02:10 -0500

    Now reading the market data from the REST endpoint instead of copypasta

--- src/libs/crypto/rest_utils.rs
+++ src/libs/crypto/rest_utils.rs
@@ -8,6 +8,14 @@ pub fn read_orders(order_book: &str, depth: i8)
    let endpoint = format!("{url}?ticker_id={order_book}&depth={depth}");
    match read_rest(&endpoint) {
       Ok(ans) => Ok(ans),
-      Err(str) => Err(format!("Error: {str:?}"))
+      Err(str) => Err(format!("Could not read orderbook {}. Error: {:?}",
+                              order_book, str))
+   }
+}
+
+pub fn read_markets() -> Result<String, String> {
+   match read_rest("https://api.kujira.app/api/coingecko/tickers") {
+      Ok(ans) => Ok(ans),
+      Err(str) => Err(format!("Could not read FIN marketplace. Error: {str:?}"))
    }
 }
