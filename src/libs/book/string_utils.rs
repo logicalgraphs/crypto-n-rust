5a6ba48e5018fb42dfcc7dba753a515f9702eb95
geophf <dauclair@hotmail.com>
2024-02-21 15:37:26 -0500

    Simplified if/else to unwrap_or()

--- src/libs/book/string_utils.rs
+++ src/libs/book/string_utils.rs
@@ -20,8 +20,8 @@ pub fn to_string(s: &str) -> String { s.to_string() }
 pub fn parse_lines<T>(f: impl Fn(String) -> Result<T, String>,
                       lines: &Vec<String>, skip_header: Option<usize>)
     -> Result<Vec<T>, String> {
-   let itr = lines.into_iter();
-   itr.skip(if let Some(n) = skip_header { n } else { 0 })
-      .map(compose!(f)(String::to_string))
-      .collect()
+   lines.into_iter()
+        .skip(skip_header.unwrap_or(0))
+        .map(compose!(f)(String::to_string))
+        .collect()
 }  
