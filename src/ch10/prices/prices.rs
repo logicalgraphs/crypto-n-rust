dd47efdc81540cca86c6d3d94988e04e0329fbde
geophf <dauclair@hotmail.com>
2024-03-04 02:38:03 -0500

    Improved sorting algorithm, applying aliasing first, then comparing by length when roots are equal (giving precedence to base tokens over LSDs).

--- src/ch10/prices/prices.rs
+++ src/ch10/prices/prices.rs
@@ -1,4 +1,7 @@
-use std::collections::HashMap;
+use std::{
+   cmp::Ordering,
+   collections::HashMap
+};
 
 use book::{
    file_utils::extract_date_and_body,
@@ -22,13 +25,20 @@ fn main() -> Result<(), String> {
       println!("asset,quote");
       let market = read_market_json()?;
       let books = parse_books(&market);
-      let mut all_prices: Vec<_> = prices(&books).into_iter().collect();
+      let all_prices1 = prices(&books);
+      let mut all_prices: Vec<_> =
+         all_prices1.into_iter()
+                    .map(|(k,v)| (alias(&aliases, &k), v))
+                    .collect();
       fn root(s: &str) -> String {
          s.trim_matches(char::is_lowercase).to_string()
       }
-      all_prices.sort_by(|(a, _), (b, _)| root(a).cmp(&root(b)));
+      fn cmp(a: &str, b: &str) -> Ordering {
+         root(a).cmp(&root(b)).then(a.len().cmp(&b.len()))
+      }
+      all_prices.sort_by(|(a, _), (b, _)| cmp(a, b));
       for (asset,price) in all_prices {
-         println!("{},{price}", alias(&aliases, &asset));
+         println!("{asset},{price}");
       }
    } else { usage(); }
    Ok(())
