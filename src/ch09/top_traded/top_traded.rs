a5e448e23832ba6e875f58a58aaf451e63d8c712
geophf <dauclair@hotmail.com>
2024-03-15 18:27:11 -0400

    Provided more readability by estimating token-volumes

--- src/ch09/top_traded/top_traded.rs
+++ src/ch09/top_traded/top_traded.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 
 use book::{
-   err_utils::ErrStr,
    html_utils::{Mode,HTML,HTML::OL,LI,mk_li,proff,h},
    list_utils::first_last,
+   num_utils::mk_estimate,
    string_utils::plural,
    utils::{get_args,pred}
 };
@@ -16,23 +16,22 @@ use crypto::{
    }
 };
 
-fn usage() -> ErrStr<()> {
+fn usage() {
    println!("\n./top_traded <date> [min volume=30000]\n");
    println!("Prints the top-traded tokens by 24h-volumes.\n");
    println!("The set of sets can be represented as a Venn diagram using, i.e.");
    println!("https://github.com/benfred/venn.js");
-   Ok(())
 }
 
-fn main() -> ErrStr<()> {
+fn main() {
    if let (Some(date), min) = first_last(&get_args()) {
-      do_it(&date, min)
+      do_it(&date, min);
    } else {
-      usage()
+      usage();
    }
 }
 
-fn do_it(date: &str, min_opt: Option<String>) -> ErrStr<()> {
+fn do_it(date: &str, min_opt: Option<String>) {
    let (_, books) = parse_books(Some(graphs_fin_res("aliases.csv")));
    let tok_vols = volumes_by_token(&books);
    let default_min: f32 = 30000.0;
@@ -48,7 +47,6 @@ fn do_it(date: &str, min_opt: Option<String>) -> ErrStr<()> {
    tok_vols.iter().for_each(print_token(min, &toks));
    println!("];");
    report(date, &toks, tok_vols);
-   Ok(())
 }
 
 fn print_token(min: f32, toks: &HashSet<String>)
@@ -79,7 +77,8 @@ fn report(date: &str, toks: &HashSet<String>, tok_vols: Volumes) {
    fn contfor(toks: &HashSet<String>)
          -> impl Fn((String, USD)) -> Option<LI> + '_ {
       | (tok, vol): (String, USD) |
-         pred(toks.contains(&tok), mk_li(&format!("{tok}: {vol}")))
+         pred(toks.contains(&tok),
+              mk_li(&format!("{tok}: ${}", mk_estimate(vol.amount))))
    }
    let toppers0: Vec<LI> = vols.into_iter().filter_map(contfor(toks)).collect();
    let toppers: Vec<LI> = toppers0.into_iter().take(10).collect();
