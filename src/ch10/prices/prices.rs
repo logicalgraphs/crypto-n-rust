64edfe6bbf5f96046baf450e90528d8256f83acc
geophf <dauclair@hotmail.com>
2024-03-25 20:59:15 -0400

    updated prices-app to use supplied library functions.

--- src/ch10/prices/prices.rs
+++ src/ch10/prices/prices.rs
@@ -1,18 +1,8 @@
-use std::cmp::Ordering;
+use crypto::{
+   algos::prices::print_sorted_prices,
+   types::books::prices_with_aliases
+};
 
-use crypto::types::books::prices_with_aliases;
+fn main() { print_sorted_prices(&prices_with_aliases()); }
 
-fn main() {
-   let all_prices1 = prices_with_aliases();
-   let mut all_prices: Vec<_> = all_prices1.into_iter().collect();
-   fn root(s: &str) -> String {
-      s.trim_matches(char::is_lowercase).to_string()
-   }
-   fn cmp(a: &str, b: &str) -> Ordering {
-      root(a).cmp(&root(b)).then(a.len().cmp(&b.len()))
-   }
-   all_prices.sort_by(|(a, _), (b, _)| cmp(a, b));
-   for (asset,price) in all_prices {
-      println!("{asset},{price}");
-   }
-}
+// ... welp.
---
+++ src/libs/crypto/algos/prices.rs
@@ -0,0 +1,28 @@
+use std::cmp::Ordering;
+
+use crate::types::{
+   books::Prices,
+   usd::USD
+};
+
+type Pair = (String, USD);
+type PairRef<'a> = (&'a String, &'a USD);
+
+pub fn merge_prices(new_prices: &Prices, portfolio: &Prices) -> Prices {
+   fn merge_price<'a>(news: &'a Prices) -> impl Fn(PairRef<'a>) -> Pair + 'a {
+      |(tok,pric)| (tok.clone(),news.get(tok).or(Some(pric)).unwrap().clone())
+   }
+   portfolio.into_iter().map(merge_price(new_prices)).collect()
+}
+
+pub fn print_sorted_prices(prices: &Prices) {
+   let mut all_prices: Vec<_> = prices.into_iter().collect();
+   fn root(s: &str) -> String {
+      s.trim_matches(char::is_lowercase).to_string()
+   }
+   fn cmp(a: &str, b: &str) -> Ordering {
+      root(a).cmp(&root(b)).then(a.len().cmp(&b.len()))
+   }
+   all_prices.sort_by(|(a, _), (b, _)| cmp(a, b));
+   all_prices.into_iter().for_each(|(asset,price)| println!("{asset},{price}"));
+}
