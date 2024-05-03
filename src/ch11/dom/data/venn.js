8644961f04fb4f47e5b1dfa3de6fd9ac9fde6c44
geophf <dauclair@hotmail.com>
2024-05-03 17:15:22 -0400

    Run off of dom cypher query and anticipated output as intersecting sets for a Venn diagram-rendering

---
+++ src/ch11/dom/data/venn.js
@@ -0,0 +1,18 @@
+var sets = [   {sets: ['Leverage', 'Trading'], size: 10603.65},
+   {sets: ['Money Market', 'HODL'], size: 21214.68},
+   {sets: ['Staking', 'HODL'], size: 18381.6426},
+   {sets: ['Trading', 'Arbitrage'], size: 16069.999},
+   {sets: ['Trading', 'Staking'], size: 68.71},
+   {sets: ['Trading', 'Arbitrage', 'LSD'], size: 1190.87},
+   {sets: ['Trading', 'Liquidations'], size: 2471.014},
+   {sets: ['Trading', 'LSD'], size: 595.43},
+   {sets: ['HODL', 'LSD'], size: 595.43},
+   {sets: ['Arbitrage'], size: 17851.499},
+   {sets: ['HODL'], size: 45167.0026},
+   {sets: ['Leverage'], size: 10732.364},
+   {sets: ['Liquidations'], size: 2471.014},
+   {sets: ['LSD'], size: 1190.87},
+   {sets: ['Money Market'], size: 21214.68},
+   {sets: ['Staking'], size: 18509.6426},
+   {sets: ['Trading'], size: 39853.889}];
+
