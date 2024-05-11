e34ec89b9941ca7938d5b3cdcff843b5ae7285eb
geophf <dauclair@hotmail.com>
2024-05-11 12:19:23 -0400

    Incorporate (optional) min-24-hr-volumes in top order books report.

--- src/ch09/top_order_books/reports.rs
+++ src/ch09/top_order_books/reports.rs
@@ -4,7 +4,10 @@ use book::{
    report_utils::{print_footer, print_top_of}
 };
 
-use crypto::types::interfaces::{Book,Books,count,vol_24h};
+use crypto::types::{
+   interfaces::{Book,Books,count,vol_24h},
+   usd::mk_usd
+};
 
 use crate::{
    analyzed_books::analyze,
@@ -12,17 +15,21 @@ use crate::{
 };
 
 pub fn reportage(date: &str, books: &Books, raw: Option<f32>) {
-   let nbooks = books.len();
    let mut alles: Vec<Book> = books.clone().into_iter().collect();
+   let nbooks = alles.len();
    alles.sort_by(|a, b| vol_24h(b).partial_cmp(&vol_24h(a)).unwrap());
    let x: Vec<Book> = alles.clone().into_iter().take(10).collect();
    let v: Vec<Book> = x.clone().into_iter().take(5).collect();
-   if let Some(min) = raw { print_alles(&alles, date, min); }
-   alles.retain(|b| vol_24h(b).amount > 100000.0);
-   let ntop = alles.len();
+   let (ntop, min) = if let Some(min) = raw {
+      print_alles(&alles, date, min)
+   } else {
+      alles.retain(|b| vol_24h(b).amount > 100000.0);
+      (alles.len(), 100000.0)
+   };
+   let min_usd = mk_usd(min);
 
-   println!("I got {} active books; {} have $100,000+ 24h-volume, {}",
-            nbooks, ntop, date);
+   println!("I got {} active books; {} have {}+ 24h-volume, {}",
+            nbooks, ntop, min_usd, date);
    count(books, "axlUSDC");
    count(books, "USK");
    count(books, "USDC");
@@ -34,11 +41,12 @@ fn print_txt<T: CsvWriter>(tops: &Vec<T>, date: &str) {
    printer(tops, date, &Mode::TEXT);
 }
 
-fn print_alles(alles: &Vec<Book>, date: &str, min: f32) {
+fn print_alles(alles: &Vec<Book>, date: &str, min: f32) -> (usize, f32) {
    let mut ballz = alles.clone();
    ballz.retain(|b| vol_24h(b).amount > min);
    println!("FIN order books by volume, {date}\n");
    println!("{}\n", list_csv(&analyze(&ballz)));
+   (ballz.len(), min)
 }
 
 fn foot(mode: &Mode) {
