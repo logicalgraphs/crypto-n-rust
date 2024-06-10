36be60602d53b968de7a4c100200c5e52cc90f89
geophf <dauclair@hotmail.com>
2024-06-10 12:14:46 -0400

    Added solution

---
+++ src/pivot/quizzes/quiz01/answer01.rs
@@ -0,0 +1,22 @@
+use book::{
+   err_utils::{err_or,ErrStr},
+   rest_utils::read_rest
+};
+
+fn usage() {
+   println!("./quiz01");
+   println!("\tReads data from a REST endpoint.\n");
+}
+
+fn main() -> ErrStr<()> {
+   usage();
+   let lg = "https://raw.githubusercontent.com/logicalgraphs";
+   let piv_data = "crypto-n-rust/pivot-quiz-01/data-files/csv/pivots.csv";
+   let res = err_or(read_rest(&format!("{lg}/{piv_data}")),
+                    "Error reading REST endpoint")?;
+   println!("First five lines of the pivots database:\n");
+   for line in res.split("\n").take(5) {
+      println!("{line}");
+   }
+   Ok(())
+}
