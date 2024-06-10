dc2880c2daf0a9a0377a0db0dc8a9232aa590d04
geophf <dauclair@hotmail.com>
2024-06-10 11:45:50 -0400

    rolled back quiz01.rs to quiz-form

--- src/pivot/quizzes/quiz01/quiz01.rs
+++ src/pivot/quizzes/quiz01/quiz01.rs
@@ -1,22 +1,14 @@
-use book::{
-   err_utils::{err_or,ErrStr},
-   rest_utils::read_rest
-};
+use book::err_utils::ErrStr;
 
 fn usage() {
    println!("./quiz01");
-   println!("\tReads data from a REST endpoint.\n");
+   println!("\tReads data from a REST endpoint.");
 }
 
 fn main() -> ErrStr<()> {
    usage();
-   let lg = "https://raw.githubusercontent.com/logicalgraphs";
-   let piv_data = "crypto-n-rust/pivot-quiz-01/data-files/csv/pivots.csv";
-   let res = err_or(read_rest(&format!("{lg}/{piv_data}")),
-                    "Error reading REST endpoint")?;
-   println!("First five lines of the pivots database:\n");
-   for line in res.split("\n").take(5) {
-      println!("{line}");
-   }
+
+   // do something here to read the REST endpoint
+   // URL: 
    Ok(())
 }
