8210eef37c4bfe92098034311711fe3160124591
geophf <dauclair@hotmail.com>
2024-06-11 14:16:36 -0400

    Updating reqwest to 0.12.4; using async to make REST-calls.

--- src/pivot/swerve/read_rest.rs
+++ src/pivot/swerve/read_rest.rs
@@ -1,14 +1,16 @@
 use book::{
    err_utils::{err_or,ErrStr},
-   rest_utils::read_rest,
    string_utils::to_string
 };
 
 use crypto::rest_utils::data_res;
 
-pub fn read_pivots() -> ErrStr<Vec<String>> {
-   let res = err_or(read_rest(&data_res("pivot-quiz-02", "pivots.csv")),
+pub async fn read_pivots() -> ErrStr<Vec<String>> {
+   let url = data_res("pivot-quiz-02", "pivots.csv");
+   let res = err_or(reqwest::get(&url).await,
+                    "Error making get-reqwest call to github")?;
+   let body = err_or(res.text().await,
                     "Error reading pivots.csv on github")?;
-   let lines: Vec<String> = res.split("\n").map(to_string).collect();
+   let lines: Vec<String> = body.split("\n").map(to_string).collect();
    Ok(lines)
 }
