92576d11fb39b424452abcac2a11647b4b6e68b0
geophf <dauclair@hotmail.com>
2024-06-13 00:44:51 -0400

    Updated read git pivots to pivot-quiz-06 branch

--- src/pivot/swerve/read_rest.rs
+++ src/pivot/swerve/read_rest.rs
@@ -7,7 +7,7 @@ use book::{
 use crypto::rest_utils::data_res;
 
 pub async fn read_pivots() -> ErrStr<Vec<String>> {
-   let url = data_res("pivot-quiz-02", "pivots.csv");
+   let url = data_res("pivot-quiz-06", "pivots.csv");
    let res = read_rest(&url).await?;
    let lines: Vec<String> = res.split("\n").map(to_string).collect();
    Ok(lines)
