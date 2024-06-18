448bf60856e8ecda22a27b1ceb8b2f48f833773b
geophf <dauclair@hotmail.com>
2024-06-17 22:14:32 -0400

    Reporting on token-prices missing from coingecko response JSON.

--- src/pivot/swerve/reports.rs
+++ src/pivot/swerve/reports.rs
@@ -1,9 +1,11 @@
+use book::{
+   csv_utils::CsvWriter,
+   string_utils::plural
+};
 
-use book::csv_utils::CsvWriter;
+use crate::types::{Diffs,Price,Token};
 
-use crate::types::{Price,Token};
-
-pub fn one_row(date: &str, row: &Vec<Price>) {
+fn one_row(date: &str, row: &Vec<Price>) {
 
    // first we show the symbols as a header:
    let syms: Vec<Token> =
@@ -12,18 +14,28 @@ pub fn one_row(date: &str, row: &Vec<Price>) {
 
    // then we print their prices:
    let vals: Vec<String> = row.iter().map(|price| price.1.as_csv()).collect();
-   println!("{date},{}", vals.join(","));
+   println!("{date},{}\n", vals.join(","));
 }
 
-pub fn portfolio_prices(date: &str, row: &Vec<Price>) {
+fn portfolio_prices(date: &str, row: &Vec<Price>) {
    println!("date,token,id,price");
    row.into_iter().for_each(|((id,sym),price)|
       println!("{date},{sym},{id},{}", price.as_csv()));
 }
 
-pub fn report(date: &str, row: &Vec<Price>) {
+fn report_diffs((kind, diffs): &Diffs) {
+   let n = diffs.len();
+   let verb = if n > 1 { "were" } else { "was" };
+   println!("There {verb} {} {}: {}",
+            plural(n, "token"), kind.as_csv(), diffs.join(", "));
+}
+
+pub fn report(date: &str, row: &Vec<Price>, errs: &Option<Diffs>) {
    println!("\nPortfolio tokens,,,\n,,,");
    portfolio_prices(date, row);
    println!("\n... and as one line:\n");
    one_row(date, row);
+   if let Some(diffs) = errs {
+      report_diffs(&diffs);
+   }
 }
--- src/pivot/swerve/types.rs
+++ src/pivot/swerve/types.rs
@@ -16,8 +16,21 @@ impl CsvWriter for Quote {
 
 pub type TokenId = String;
 pub type Token = String;
+pub type RawPrices = HashMap<TokenId, Quote>;
 pub type Dict = HashMap<TokenId, Token>;
 
 pub type Pivots = Vec<String>;
 
 pub type Price = ((TokenId, Token), Quote);
+
+#[derive(PartialEq)]
+pub enum Diff { MISSING, ADDED }
+
+impl CsvWriter for Diff {
+   fn as_csv(&self) -> String {
+      (if self == &Diff::MISSING { "missing" } else { "added"}).to_string()
+   }
+   fn ncols(&self) -> usize { 1 }
+}
+
+pub type Diffs = (Diff, Vec<String>);
