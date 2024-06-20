f15eee05d700448d519d892edea6a93581ec3722
geophf <dauclair@hotmail.com>
2024-06-20 02:15:23 -0400

    Using NaiveDate to parse dates.

--- src/pivot/quizzes/quiz08/answer08.rs
+++ src/pivot/quizzes/quiz08/answer08.rs
@@ -1,4 +1,10 @@
-use book::err_utils::ErrStr;
+use chrono::{NaiveDate}; // ,format::parse_and_remainder};
+
+use book::{
+   err_utils::{err_or,ErrStr},
+   list_utils::{parse_nums,tail},
+   string_utils::to_string
+};
 
 use swerve::snarf::snarf_pivots;
 
@@ -13,8 +19,19 @@ async fn main() -> ErrStr<()> {
    let (pivs, _dict) = snarf_pivots().await?;
    if let Some(line) = pivs.last() {
       println!("My last pivot is {line}");
+      let (date, nums) = parse_row(&line)?;
+      println!("Date: {date}");
+      println!("pivots: {nums:?}");
       Ok(())
    } else {
       Err("No last pivot!".to_string())
    }
+}
+
+fn parse_row(row: &str) -> ErrStr<(NaiveDate, Vec<f32>)> {
+   let (date, line) = err_or(NaiveDate::parse_and_remainder(row, "%Y-%m-%d"),
+                             &format!("Unable to parse date from '{row}'"))?;
+   let cols: Vec<String> = line.split(",").map(to_string).collect();
+   let nums = parse_nums(tail(&cols));
+   Ok((date, nums))
 }
