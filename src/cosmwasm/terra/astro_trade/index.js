d98516bfb02acea7ce13a2f135c6389cb80dd0b6
geophf <dauclair@hotmail.com>
2024-07-11 11:30:09 -0400

    WIP: I'm of two minds here. Should I bite the bullet and deploy for testnet or integrate mainnet from the get-go?

---
+++ src/cosmwasm/terra/astro_trade/index.js
@@ -0,0 +1,13 @@
+import { LCDClient } from '@terra-money/feather.js';
+
+const lcd = new LCDClient({
+  // key must be the chainID
+  'pisco-1': {
+    lcd: 'https://pisco-lcd.terra.dev',
+    chainID: 'pisco-1',
+    gasAdjustment: 1.75,
+    gasPrices: { uluna: 0.015 },
+    prefix: 'terra', // bech32 prefix, used by the LCD to understand which is the right chain to query
+  },
+});
+
