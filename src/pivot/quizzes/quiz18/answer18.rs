5e4fe6e84f1d56661830617e84b615145badbf9a
geophf <dauclair@hotmail.com>
2024-08-27 11:03:26 -0400

    WIP: started solution by copypasta from quiz17

---
+++ src/pivot/quizzes/quiz18/answer18.rs
@@ -0,0 +1,19 @@
+use book::{
+   csv_utils::print_csv,
+   err_utils::ErrStr,
+   table_utils::{from_map, transpose}
+};
+
+use swerve::{
+   fetch_prices::read_chart_from_file,
+   types::StampedData
+};
+
+fn main() -> ErrStr<()> {
+   let chart = read_chart_from_file("../quiz17/data/eth.json")?;
+   let stamped_prices: &StampedData<f64> = chart.get("prices").expect("price");
+   let table = from_map("ETH", stamped_prices);
+   let transposed = transpose(&table);
+   print_csv(&transposed);
+   Ok(())
+}
