6f0a1ebc533eb8e189e272929c953892be68f5f5
pivoteur <dauclair@hotmail.com>
2026-01-09 18:55:34 -0500

    added user-agent to fetch-prices request to coingecko

--- src/pivot/swerve/fetch_prices.rs
+++ src/pivot/swerve/fetch_prices.rs
@@ -29,6 +29,7 @@ async fn gecko_fetcher(auth: &str, url: &str, params: &[(&str, &str)])
    let req = client.get(url)
                    .query(params)
                    .header("accept", "application/json")
+                   .header("user-agent", "bae")
                    .header("x-cg-demo-api-key", auth);
    let res = err_or(req.send().await, "sending GET request to coingecko")?;
    let json = err_or(res.text().await, "parsing result body from coingecko")?;
