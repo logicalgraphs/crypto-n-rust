f3e8b9be1e329177f2f5d4a635eb1793b2bb8d67
pivoteur <dauclair@hotmail.com>
2026-03-03 10:13:11 -0500

    added server.js to repository, 2026-03-03

---
+++ src/scripts/server.js
@@ -0,0 +1,31 @@
+// add server.js to the parent directory of protocols to run
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const args = process.argv.slice(2);
+
+Object.defineProperty(Array.prototype, 'first', {
+  value() {
+    return this.find(Boolean)
+  }
+});
+
+const dispatch = args.first() || 'pivoteur';
+const appName = dispatch + '.github.io';
+const vennDir = appName + '/.git/modules/charts';
+
+app.use(express.static(path.join(__dirname, appName)));
+// app.use('/data', express.static(path.join(__dirname, 'data')));
+// app.use('/imgs', express.static(path.join(__dirname, 'imgs')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, appName, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('App is', appName);
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
