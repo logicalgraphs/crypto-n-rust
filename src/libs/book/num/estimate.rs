033e28b0796a06562870f6a8ff42be632bc0c80d
pivoteur <dauclair@hotmail.com>
2026-03-14 13:38:30 -0400

    Added unit tests for estimate; also added approximate-fn, 2026-03-13

--- src/libs/book/num/estimate.rs
+++ src/libs/book/num/estimate.rs
@@ -62,3 +62,84 @@ impl fmt::Display for Estimate {
       write!(formatter, "{mantissa:.2}{exponent}")
    }
 }
+
+impl Estimate {
+   pub fn approximates(&self, n: f32) -> bool {
+      let base = self.val;
+      let e = base * 0.05;
+      base - e < n && n < base + e
+   }
+}
+
+// ----- TESTS --------------------------------------------------------------
+
+pub mod functional_tests {
+   use super::*;
+   use crate::string_utils::words;
+
+   pub fn runoff() -> ErrStr<usize> {
+      println!("num::estimate functional test\n");
+      let nums = words("15.53 1247.9 5614723.99");
+      let ests: Vec<Estimate> =
+         nums.iter()
+             .filter_map(|s| s.parse().ok()
+                              .or_else(|| panic!("Cannot parse {s}")))
+             .collect();
+      let strs: Vec<String> = ests.iter().map(|s| format!("{s}")).collect();
+      println!("numbers:\t{}", nums.join("\t"));
+      println!("estimates:\t{}", strs.join("\t"));
+      Ok(1)
+   }
+}
+
+#[cfg(test)]
+mod tests {
+   use super::*;
+
+   #[test]
+   fn fail_parse_estimate() {
+      let ans: ErrStr<Estimate> = "123adfkl".parse();
+      assert!(ans.is_err());
+   }
+
+   #[test]
+   fn test_parse_estimate_ok() {
+      let ans: ErrStr<Estimate> = "15.35".parse();
+      assert!(ans.is_ok());
+      assert_eq!(15.35, ans.unwrap().val);
+   }
+
+   #[test]
+   fn test_parse_estimate_k() -> ErrStr<()> {
+      let ans: Estimate = "12k".parse()?;
+      let est = ans.val;
+      assert!(11000.0 < est && est < 13000.0);
+      Ok(())
+   }
+
+   #[test]
+   fn test_parse_estimate_m() -> ErrStr<()> {
+      let ans: Estimate = "592.97M".parse()?;
+      let val = 592974111.0;
+      assert!(ans.approximates(val));
+      Ok(())
+   }
+
+   #[test]
+   fn test_approximates_exact() {
+      let est = mk_estimate(15.35);
+      assert!(est.approximates(15.35));
+   }
+
+   #[test]
+   fn test_approximates_k() {
+      let est = mk_estimate(54627.0);
+      assert!(est.approximates(54630.0));
+   }
+
+   #[test]
+   fn test_approximates_m() {
+      let est = mk_estimate(123456789.0);
+      assert!(est.approximates(120.0 * 1e06));
+   }
+}
--- src/libs/book/src/main.rs
+++ src/libs/book/src/main.rs
@@ -4,14 +4,16 @@ use book::{
    string_utils::functional_tests::runoff as stri,
    date_utils::functional_tests::runoff as d,
    currency::usd::functional_tests::runoff as u,
+   num::estimate::functional_tests::runoff as e,
+
    err_utils::ErrStr,
    test_utils::{collate_results,mk_tests,mk_sync}
 }; 
 
 fn main() -> ErrStr<()> {
    let _ = collate_results("book",
-      & mut mk_tests("string_utils date_utils currency::usd",
-                     vec![mk_sync(stri), mk_sync(d), mk_sync(u)]))?;
+      & mut mk_tests("string_utils date_utils currency::usd num::estimate",
+                     vec![mk_sync(stri), mk_sync(d), mk_sync(u), mk_sync(e)]))?;
    Ok(())
 }
 
