7bb3a27a1319bd85f3a2b96b3859d7f9bdccb1a7
pivoteur <dauclair@hotmail.com>
2026-04-22 10:28:55 -0400

    made filter_map_or public

--- src/libs/book/table_utils.rs
+++ src/libs/book/table_utils.rs
@@ -133,7 +133,7 @@ pub fn ingest<ROW: Eq + Hash,COL: Eq + Hash,DATUM>
    }
 }
 
-fn filter_map_or<T>(f: impl Fn(&str) -> ErrStr<T>, v: &Vec<String>)
+pub fn filter_map_or<T>(f: impl Fn(&str) -> ErrStr<T>, v: &Vec<String>)
       -> ErrStr<Vec<T>> {
    let mut ans: Vec<T> = Vec::new();
    for elt in v {
